4a6a38904bd0fd933c9595d1173ffa66571da1d8
root <gwleong@gmail.com>
2015-11-04 23:32:57 +0000

    Automatically committed by jiffy on Wed, 04 Nov 2015 23:32:57 +0000

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 PKGNAME=$1
 
 mkdir /var/tmp/jiffy/temp
-tar xvfz /var/tmp/jiffy/$PKGNAME -C /var/tmp/jiffy/temp
-ln -s /var/tmp/jiffy/temp /var/chef-solo
-/var/chef-solo/run_order
-rm -rf /var/chef-solo
+tar xvfz /var/tmp/jiffy/$PKGNAME -C /var/tmp/jiffy/temp && \
+ln -s /var/tmp/jiffy/temp /var/chef-solo && \
+/var/chef-solo/run_order && \
+rm -rf /var/chef-solo && \
 rm -rf /var/tmp/jiffy/temp
